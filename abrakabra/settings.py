e9b651dd81130bd8089d66a02e8589afb8d2018c
Unknown <maksimmerzh@gmail.com>
2018-03-19 19:24:53 +0300

    add static_root

--- abrakabra/settings.py
+++ abrakabra/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'abrakabra.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,3 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
